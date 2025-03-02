eb4a4098ed579fe00d049f22e73fa1f38d4dd933
Aaron <landotek@proton.me>
2025-03-02 21:09:55 +0900

    configured cmake for cross-platform project structure

---
+++ algorithms/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/aaron/GitHub/DataStructures-Algorithms/algorithms
+# Build directory: C:/Users/aaron/GitHub/DataStructures-Algorithms/algorithms/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ algorithms/src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <unordered_map>
+
+unordered_map<char, int> huffmanCompression(std::string inputString)
+{
+    
+}
+
+int main()
+{
+  std::cout << "Hello Windows\n";
+  return 0;
+}
